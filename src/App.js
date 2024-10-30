7d8081d782839ea99ad87224be2731178e1c1461
pradeepdedsec <pradeepdedsec@gmail.com>
2024-10-30 23:25:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleClick =async ()=>{
+    try {
+      let res = await fetch("https://node-api-test-8hlf.onrender.com/", {
+        method: 'GET',
+        credentials: 'include'
+    });      if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+      let data = await res.json();
+      console.log(data);
+  } catch (error) {
+      console.error("Fetch error:", error);
+  }
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"> 
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleClick}> click</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
